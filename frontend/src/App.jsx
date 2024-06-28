f2bb32a36b98f0b2d1a077743b654f5324cdb7db
Sudhanshu-Sagar <kashyaprudra8507@gmail.com>
2024-06-28 21:54:15 +0530

    cart page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,15 @@ import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 import Footer from './component/Footer/Footer'
+import { useState } from 'react'
+import LoginPopup from './component/LoginPopup/LoginPopup'
 const App = () => {
+  const [showLogin,setShowLogin]=useState(false)
   return (
     <>
+    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
     <div className='app'>
-      <Navbar />
+      <Navbar setShowLogin={setShowLogin}/>
       <Routes>
         <Route path="/" element={<Home/>}></Route>
         <Route path="/cart" element={<Cart/>}></Route>
--- frontend/src/component/navbar/Navbar.jsx
+++ frontend/src/component/navbar/Navbar.jsx
@@ -2,16 +2,16 @@ import  { useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/frontend_assets/assets'
 import {Link} from 'react-router-dom'
-const Navbar = () => {
+const Navbar = ({setShowLogin}) => {
     const [menu,setMenu]=useState("menu"); //this is used so that when user click on any list the underline is created on that list
     return (
         <div className="navbar">
             <img src={assets.logo} />
             <ul className='navbar-menu'>
-                <Link to='/'onClick={()=>setMenu("home")}className={menu==="home"?"active":""}>home</Link>
-                <a href='#explore-menu'onClick={()=>setMenu("menu")}className={menu==="menu"?"active":""}>menu</a>
-                <a href='#app-download'onClick={()=>setMenu("mobile-app")}className={menu==="mobile-app"?"active":""}>mobile-app</a>
-                <a href='#footer' onClick={()=>setMenu("contact-us")}className={menu==="contact-us"?"active":""}>contact-us</a>
+                <Link to='/' onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>home</Link>
+                <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</a>
+                <a href='#app-download' onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</a>
+                <a href='#footer' onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact-us</a>
             </ul>
             <div className='navbar-right'>
                 <img src={assets.search_icon} />
@@ -19,7 +19,7 @@ const Navbar = () => {
                     <img src={assets.basket_icon} />
                     <div className='dot'> </div> {/*this dot is used when user add something in the basket */} 
                 </div>
-                <button>sign-in</button>
+                <button onClick={()=>setShowLogin(true)}>sign-in</button>
 
             </div>
         </div>
