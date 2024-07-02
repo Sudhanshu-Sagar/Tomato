e4d940b2c782e0509124dfca0a2715776b9f96cc
Sudhanshu-Sagar <kashyaprudra8507@gmail.com>
2024-07-02 12:24:47 +0530

    cart page parcel page

--- frontend/src/component/navbar/Navbar.jsx
+++ frontend/src/component/navbar/Navbar.jsx
@@ -1,12 +1,14 @@
-import  { useState } from 'react'
+import  { useContext, useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/frontend_assets/assets'
 import {Link} from 'react-router-dom'
+import { StoreContext } from '../../context/StoreContext'
 const Navbar = ({setShowLogin}) => {
     const [menu,setMenu]=useState("menu"); //this is used so that when user click on any list the underline is created on that list
+    const {getTotalCartAmount}=useContext(StoreContext);
     return (
         <div className="navbar">
-            <img src={assets.logo} />
+        <Link to="/"><img src={assets.logo} /></Link>
             <ul className='navbar-menu'>
                 <Link to='/' onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>home</Link>
                 <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</a>
@@ -16,8 +18,8 @@ const Navbar = ({setShowLogin}) => {
             <div className='navbar-right'>
                 <img src={assets.search_icon} />
                 <div className='navbar-search-icon'>
-                    <img src={assets.basket_icon} />
-                    <div className='dot'> </div> {/*this dot is used when user add something in the basket */} 
+                   <Link to="/cart"> <img src={assets.basket_icon} /></Link>
+                    <div className={getTotalCartAmount()===0?"":"dot"}> </div> {/*this dot is used when user add something in the basket */} 
                 </div>
                 <button onClick={()=>setShowLogin(true)}>sign-in</button>
 
--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -7,7 +7,7 @@ export const StoreContext = createContext(null)
 
 const StoreContextProvider= (props) =>{
      const [cartItems,setCartItems]=useState({});
-     
+
      const addToCart=(itemId)=>{
         if(!cartItems[itemId]){
             setCartItems((prev)=>({...prev,[itemId]:1}))
@@ -25,12 +25,27 @@ const StoreContextProvider= (props) =>{
         console.log(cartItems);
      },[cartItems])
 
+
+     const getTotalCartAmount=()=>{
+        let totalAmount=0;
+        for(const item in cartItems)
+            {
+                
+                if(cartItems[item]>0)
+                    {
+                        let itemInfo=food_list.find((product)=>product._id===item);
+                     totalAmount+=itemInfo.price*cartItems[item];
+                    }
+            }
+            return totalAmount;
+     }
     const contextValue={
         food_list,
         cartItems,
         setCartItems,
         addToCart,
-        removeFromCart
+        removeFromCart,
+        getTotalCartAmount
        }
    
     return(
