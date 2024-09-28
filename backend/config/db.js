d5e83149dc4dd3d11f27cd144df1fab786d23826
Sudhanshu-Sagar <kashyaprudra8507@gmail.com>
2024-09-29 02:08:15 +0530

    final deploy

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
-import mongoose  from "mongoose";
-export const conn=async()=>{
-    await mongoose.connect('mongodb+srv://sudhanshusagar077:7277541915@cluster0.cynuwvv.mongodb.net/food-del').then(()=>{
-        console.log("db connected")
-    })
-}
+import mongoose from "mongoose";
+
+ export const connectDB = async () => {
+    await mongoose.connect('mongodb+srv://sudhanshusagar077:7277541915@cluster0.cynuwvv.mongodb.net/food-del').then(() =>console.log("DB Connected"));
+    
+}        
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,17 +1,21 @@
-import ExploreMenu from '../../component/ExploreMenu/ExploreMenu';
-import Header from '../../component/Header/Header'
-import './Home.css';
-import FoodDisplay from '../../component/FoodDisplay/FoodDisplay';
-import {useState} from 'react'
-import AppDownload from '../../component/AppDownload/AppDownload';
+import React, { useState } from 'react'
+import './Home.css'
+import Header from '../../components/Header/Header'
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
+import AppDownload from '../../components/AppDownload/AppDownload'
+
 const Home = () => {
-    const [category,setCategory]=useState("All");
+
+    const[category,setCategory]=useState("All");
+
   return (
     <div>
-      <Header/>
-      <ExploreMenu category={category} setCategory={setCategory}/>
-      <FoodDisplay category={category}/>
-      <AppDownload/>
+
+    <Header/>
+    <ExploreMenu category={category} setCategory={setCategory}/>
+    <FoodDisplay category={category}/>
+    <AppDownload/>
     </div>
   )
 }
